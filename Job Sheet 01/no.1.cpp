b8dbf348e41b55a8188d99ab043de61247829dfa
Muhammad Firman <150314915+MuhammadFirmannn@users.noreply.github.com>
2023-12-14 23:16:04 +0700

    Add files via upload

---
+++ Job Sheet 01/no.1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Menampilkan pesan pertama
+    printf("Hello, siapa nama lengkapmu?\n");
+
+    // Deklarasi variabel untuk menyimpan nama
+    int nama[100];
+
+    // Menerima input nama dari pengguna
+    scanf("%[^\n]s", nama);
+
+    // Menampilkan pesan selamat datang
+    printf("Selamat Datang %s dalam Pemrograman C!\n", nama);
+
+    return 0;
+}
+
