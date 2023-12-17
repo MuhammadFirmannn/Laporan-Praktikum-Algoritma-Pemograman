28aeeb936137085cdb991367f542aa824de9affd
Muhammad Firman <150314915+MuhammadFirmannn@users.noreply.github.com>
2023-12-17 17:50:58 +0700

    Add files via upload

---
+++ Job sheet 09/Contoh coding/contoh 3.cpp
@@ -0,0 +1,20 @@
+/*
+Nama  : Muhammad Firman
+NIM   : 23343044
+Prodi : Informatika
+*/
+
+#include <stdio.h>
+
+void add_score(int *score){
+	
+	*score = *score + 5;
+}
+
+int main(){
+	int score = 0;	
+	printf("score sebelum diubah: %d\n", score);
+	add_score(&score);
+	printf("score setelah diubah: %d\n", score);
+
+}
