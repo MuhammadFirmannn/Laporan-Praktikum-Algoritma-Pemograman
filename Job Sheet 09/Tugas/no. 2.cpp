4f538b2bcda8a1df62f455a962012727c6ce2713
Muhammad Firman <150314915+MuhammadFirmannn@users.noreply.github.com>
2023-12-17 18:36:00 +0700

    Add files via upload

---
+++ Job Sheet 09/Tugas/no. 2.cpp
@@ -0,0 +1,25 @@
+/*
+Nama  : Muhammad Firman
+NIM   : 23343044
+Prodi : Informatika
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[25];
+
+    printf("Masukan Teks: ");
+    scanf("%[^\n]", name);
+
+    int length = strlen(name);
+    char* ptr = name + length - 1;
+
+    for (; ptr >= name; ptr--) {
+        printf("%c", *ptr);
+    }
+    printf("\n\n");
+    
+    return 0;
+}
